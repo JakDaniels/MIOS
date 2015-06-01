d226c218cab299ad3d6740cded9791bb4f575136
Jak Daniels <jak@ateb.co.uk>
2015-06-01 14:02:04 +0100

    Testing the use of --server option with mono

--- php_inc/os_exec.sh
+++ php_inc/os_exec.sh
@@ -4,4 +4,4 @@ export MONO_THREADS_PER_CPU=2048
 export MONO_GC_PARAMS=nursery-size=64m
 
 cd ~/opensim/bin
-exec mono OpenSim.exe -inimaster="$1" -inifile="$2"
+exec mono --server OpenSim.exe -inimaster="$1" -inifile="$2"
