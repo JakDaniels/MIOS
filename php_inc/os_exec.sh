f4450223c8491d63dca45b22510d15b9b1e64773
Jak Daniels <jak@ateb.co.uk>
2016-09-07 12:59:51 +0100

    fix typo

--- php_inc/os_exec.sh
+++ php_inc/os_exec.sh
@@ -5,4 +5,4 @@ export MONO_GC_PARAMS=nursery-size=128m
 export MONO_ENABLE_COOP=true
 
 cd ~/opensim/bin
-exec mono -gc=sgen OpenSim.exe -inimaster="$1" -inifile="$2"
+exec mono --gc=sgen OpenSim.exe -inimaster="$1" -inifile="$2"
