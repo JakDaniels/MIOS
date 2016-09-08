b80ad034076cf95e3bd77b09384d33fc33f71e99
Jak Daniels <jak@ateb.co.uk>
2016-09-08 16:14:22 +0100

    and more playing with the GC

--- php_inc/os_exec.sh
+++ php_inc/os_exec.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 export MONO_THREADS_PER_CPU=4096
-export MONO_GC_PARAMS="nursery-size=4m,soft-heap-limit=1024m,evacuation-threshold=90,save-target-ratio=0.1,default-allowance-ratio=1.0"
+#export MONO_GC_PARAMS="nursery-size=4m,soft-heap-limit=1024m,evacuation-threshold=90,save-target-ratio=0.1,default-allowance-ratio=1.0"
+export MONO_GC_PARAMS="nursery-size=128m"
 
 cd ~/opensim/bin
-exec mono --gc=sgen OpenSim.exe -inimaster="$1" -inifile="$2"
+#exec mono --gc=sgen OpenSim.exe -inimaster="$1" -inifile="$2"
+exec mono --desktop OpenSim.exe -inimaster="$1" -inifile="$2"
